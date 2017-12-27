c0bc74affc8750a0874b596afa0cd8617a6b9367
GuanYQ0926 <guanyuqing0926@icloud.com>
2017-12-27 16:21:07 +0900

    azimuth speed

--- src/main.js
+++ src/main.js
@@ -36,7 +36,7 @@ AFRAME.registerComponent('sky-view', {
     tick: function(time, timeDelta) {
         const d = timeDelta*this.data.sunspeed
         this.data.inclination += d
-        this.data.azimuth += (d/5)
+        this.data.azimuth += (d/7)
         const theta = Math.PI*(this.data.inclination-0.5)
         const phi = 2*Math.PI*(this.data.azimuth-0.5)
         this.el.setAttribute('material', 'sunPosition', {
